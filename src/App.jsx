c289dc8d9561285c293ba000391d2f6e86626eb0
Isham17NIT <ishamahuja17@gmail.com>
2025-06-28 19:17:34 +0530

    Fixed favorites Navbar and favorites page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 function App() {
   return(
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/favorites" element={<Favorites/>}/>
       </Routes>
     </>
   )
